38f926268f451af16f9f7f094cc850d183a14085
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-02-05 18:14:01 +0200

    add load static

--- jumia/urls.py
+++ jumia/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'jumia'
 
 urlpatterns = [
-    path('' , views.JumiaList.as_view , name='jumia_list'),
-    path('<slug:slug>', views.JumiaDetail.as_view , name='jumia_detail')
+    path('' , views.JumiaList.as_view() , name='jumia_list'),
+    path('<slug:slug>', views.JumiaDetail.as_view() , name='jumia_detail')
 ]
