9919f845b069021189736807cfdd184a120068e8
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-04-11 23:49:22 +0200

    add id

--- project/tempCodeRunnerFile.py
+++ project/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-TEXT
+
+# def main_category(f):
+#             conn , cur = connn()
+#             c = 0
+#             print(f)
+#             list = [['laptops'],['mobile-phones'],['mobile-accessories'],['electronic-television-video'],['cameras'],['home-audio-electronics'],['electronics-headphone'],['computer-data-storage'],['computing-accessories'],['computer-components'],['computer-accessories'],['networking'],['pc-gaming'],['laystation-games'],['digital-games'],['xbox-games']]
+#             for items in list:
+#                   print(items)
+#                   cur.execute("INSERT INTO MAIN_CATEGORY (categories) values (%s)",(items))
+#                   conn.commit()
+#             conn.close()
+# main_category(y)
